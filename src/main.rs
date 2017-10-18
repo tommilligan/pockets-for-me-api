1d5d5cd3233857e78afd2bbbc77618a7ee81ecea
Tom Milligan <tom@tommilligan.net>
2017-10-18 19:29:05 +0100

    Working items JSON mapping. Broken tests

--- src/main.rs
+++ src/main.rs
@@ -8,58 +8,37 @@ extern crate rocket;
 #[cfg(test)] mod tests;
 
 use rocket_contrib::{Json, Value};
-use rocket::State;
-use std::collections::HashMap;
-use std::sync::Mutex;
 
-// The type to represent the ID of a message.
-type ID = usize;
-
-// We're going to store all of the messages here. No need for a DB.
-type MessageMap = Mutex<HashMap<ID, String>>;
+type ElasticId = String;
 
 #[derive(Serialize, Deserialize)]
-struct Message {
-    id: Option<ID>,
-    contents: String
+struct Item {
+    category: String,
+    make: String,
+    model: String,
+    version: String,
+    name: String,
+    description: String,
+    dimensions: [i32; 3]
 }
 
-// TODO: This example can be improved by using `route` with multiple HTTP verbs.
-#[post("/<id>", format = "application/json", data = "<message>")]
-fn new(id: ID, message: Json<Message>, map: State<MessageMap>) -> Json<Value> {
-    let mut hashmap = map.lock().expect("map lock.");
-    if hashmap.contains_key(&id) {
-        Json(json!({
-            "status": "error",
-            "reason": "ID exists. Try put."
-        }))
-    } else {
-        hashmap.insert(id, message.0.contents);
-        Json(json!({ "status": "ok" }))
-    }
+#[post("/", format = "application/json", data = "<item>")]
+fn item_create(item: Json<Item>) -> Json<Value> {
+    Json(json!({
+        "status": "success",
+        "reason": "We always win when creating!"
+    }))
 }
 
-#[put("/<id>", format = "application/json", data = "<message>")]
-fn update(id: ID, message: Json<Message>, map: State<MessageMap>) -> Option<Json<Value>> {
-    let mut hashmap = map.lock().unwrap();
-    if hashmap.contains_key(&id) {
-        hashmap.insert(id, message.0.contents);
-        Some(Json(json!({ "status": "ok" })))
-    } else {
-        None
-    }
+#[get("/<id>")]
+fn item_get(id: ElasticId) -> Json<Value> {
+    Json(json!({
+        "status": "success",
+        "reason": "We always win when getting!"
+    }))
 }
 
-#[get("/<id>", format = "application/json")]
-fn get(id: ID, map: State<MessageMap>) -> Option<Json<Message>> {
-    let hashmap = map.lock().unwrap();
-    hashmap.get(&id).map(|contents| {
-        Json(Message {
-            id: Some(id),
-            contents: contents.clone()
-        })
-    })
-}
+
 
 #[error(404)]
 fn not_found() -> Json<Value> {
@@ -71,9 +50,8 @@ fn not_found() -> Json<Value> {
 
 fn rocket() -> rocket::Rocket {
     rocket::ignite()
-        .mount("/message", routes![new, update, get])
+        .mount("/items", routes![item_create, item_get])
         .catch(errors![not_found])
-        .manage(Mutex::new(HashMap::<ID, String>::new()))
 }
 
 fn main() {
