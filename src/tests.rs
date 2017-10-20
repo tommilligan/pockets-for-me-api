f18327761c6414117c4a19dbafb23d070bed9864
Tom Milligan <tom@tommilligan.net>
2017-10-20 19:24:39 +0100

    Added tests for items. Horrific duplication

--- src/tests.rs
+++ src/tests.rs
@@ -1,72 +1,94 @@
+
 use rocket;
 use rocket::local::Client;
 use rocket::http::{Status, ContentType};
 
+extern crate serde_json;
+
+
+
+#[derive(Debug, Serialize, Deserialize)]
+enum ItemCategories {
+    Phone,
+    Tablet
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ItemElastic {
+    category: ItemCategories,
+    description: String,
+    dimension_x: i32,
+    dimension_y: i32,
+    dimension_z: i32,
+    make: String,
+    model: String,
+    name: String,
+    version: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct CreatedResponse {
+    id: String
+}
+
 #[test]
-fn bad_get_put() {
+fn post_get() {
     let client = Client::new(rocket()).unwrap();
 
-    // Try to get a message with an ID that doesn't exist.
-    let mut res = client.get("/message/99").header(ContentType::JSON).dispatch();
-    assert_eq!(res.status(), Status::NotFound);
-
-    let body = res.body_string().unwrap();
-    assert!(body.contains("error"));
-    assert!(body.contains("Resource was not found."));
-
-    // Try to get a message with an invalid ID.
-    let mut res = client.get("/message/hi").header(ContentType::JSON).dispatch();
-    let body = res.body_string().unwrap();
-    assert_eq!(res.status(), Status::NotFound);
-    assert!(body.contains("error"));
-
-    // Try to put a message without a proper body.
-    let res = client.put("/message/80").header(ContentType::JSON).dispatch();
-    assert_eq!(res.status(), Status::BadRequest);
-
-    // Try to put a message for an ID that doesn't exist.
-    let res = client.put("/message/80")
+    // Add a new item and get its id
+    let mut res = client.post("/items")
         .header(ContentType::JSON)
-        .body(r#"{ "contents": "Bye bye, world!" }"#)
+        .body(r#"{
+            "category": "Phone",
+            "make": "Apple",
+            "model": "iPhone",
+            "version": "4",
+            "description": "Apple's last truly reliable phone; the Nokia brick of the smartphone era",
+            "dimensions": [59, 116, 10]
+        }"#)
         .dispatch();
 
+    assert_eq!(res.status(), Status::Created);
+    let body = res.body().unwrap().into_string().unwrap();
+    println!("{}", &body);
+    let j: CreatedResponse = serde_json::from_str(&body).unwrap();
+
+    // Check we got an id back, and use t to get the created object back
+    let created_id = j.id;
+    let endpoint = format!("/items/{}", created_id);
+
+    // Check that the item exists with the correct contents
+    let mut res = client.get(endpoint).header(ContentType::JSON).dispatch();
+    assert_eq!(res.status(), Status::Ok);
+    let body = res.body().unwrap().into_string().unwrap();
+    let j: ItemElastic = serde_json::from_str(&body).unwrap();
+
+    assert_eq!(j.model, "iPhone");
+    assert_eq!(j.dimension_x, 116);
+}
+
+#[test]
+fn bad_get() {
+    let client = Client::new(rocket()).unwrap();
+    let res = client.get("/items/spam").header(ContentType::JSON).dispatch();
     assert_eq!(res.status(), Status::NotFound);
 }
 
 #[test]
-fn post_get_put_get() {
+fn bad_post_category() {
     let client = Client::new(rocket()).unwrap();
 
-    // Check that a message with ID 1 doesn't exist.
-    let res = client.get("/message/1").header(ContentType::JSON).dispatch();
-    assert_eq!(res.status(), Status::NotFound);
-
-    // Add a new message with ID 1.
-    let res = client.post("/message/1")
+    // Add a new item with invalid category
+    let res = client.post("/items")
         .header(ContentType::JSON)
-        .body(r#"{ "contents": "Hello, world!" }"#)
+        .body(r#"{
+            "category": "phine",
+            "make": "Apple",
+            "model": "iPhone",
+            "version": "4",
+            "description": "Apple's last truly reliable phone; the Nokia brick of the smartphone era",
+            "dimensions": [59, 116, 10]
+        }"#)
         .dispatch();
-
-    assert_eq!(res.status(), Status::Ok);
-
-    // Check that the message exists with the correct contents.
-    let mut res = client.get("/message/1").header(ContentType::JSON).dispatch();
-    assert_eq!(res.status(), Status::Ok);
-    let body = res.body().unwrap().into_string().unwrap();
-    assert!(body.contains("Hello, world!"));
-
-    // Change the message contents.
-    let res = client.put("/message/1")
-        .header(ContentType::JSON)
-        .body(r#"{ "contents": "Bye bye, world!" }"#)
-        .dispatch();
-
-    assert_eq!(res.status(), Status::Ok);
-
-    // Check that the message exists with the updated contents.
-    let mut res = client.get("/message/1").header(ContentType::JSON).dispatch();
-    assert_eq!(res.status(), Status::Ok);
-    let body = res.body().unwrap().into_string().unwrap();
-    assert!(!body.contains("Hello, world!"));
-    assert!(body.contains("Bye bye, world!"));
+    assert_eq!(res.status(), Status::BadRequest);
 }
